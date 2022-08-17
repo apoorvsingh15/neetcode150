81055b1c744df48653f24fff500fa68e7ed1cfb1
Apoorv Singh <apoorv.singh15@gmail.com>
2022-08-17 20:41:45 +0530

    added alternate solution to contains Duplicates

--- arrays-and-strings/containsDuplicates.js
+++ arrays-and-strings/containsDuplicates.js
@@ -5,17 +5,18 @@
 // Space Complexity - O(n)
 // Time Complexity - O(n)
 
+// Best solution
 function containsDuplicate(nums) {
     // Make a new Set
     let hashSet = new Set();
     let containsDuplicate = false;
 
     // Iterate through the array - O(n)
-    for(i=0; i<nums.length; i++){
+    for (i = 0; i < nums.length; i++) {
         // If set contains duplicates
-        if(hashSet.has(nums[i])){
+        if (hashSet.has(nums[i])) {
             containsDuplicate = true;
-        }else{
+        } else {
             // add the element to the set
             hashSet.add(nums[i]);
         }
@@ -25,6 +26,23 @@ function containsDuplicate(nums) {
 
 };
 
-const result = containsDuplicate([1,2,3,4]);
+const result = containsDuplicate([1, 2, 3, 4]);
 
 console.log(result);
+
+// Alternate Solution
+// Space Complexity - O(nlogn)
+// Time Complexity - O(1)
+function containsDuplicateAlt(nums) {
+    nums.sort();
+
+    for (i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) return true;
+    }
+    return false;
+};
+
+const altResult = containsDuplicateAlt([1, 1, 2, 2]);
+const altResultNegative = containsDuplicateAlt([]);
+
+console.log(altResult, altResultNegative)
